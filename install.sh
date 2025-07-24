0c49b3a45493265b7477af997d0a39ee2a6a13fe
mb <keksh01@gmail.com>
2025-07-24 20:01:24 +0200

    Handle error if chsh fails

--- install.sh
+++ install.sh
@@ -126,6 +126,10 @@ if [[ "$SHELL" != "/bin/zsh" ]]; then
     printf "\n"
     info "Setting zsh as the default shell..."
     chsh -s $(which zsh)
+    if [[ $? -ne 0 ]]; then
+        error "Failed to change default shell to zsh"
+        exit 1
+    fi
     source ~/.zshrc
 fi
 
