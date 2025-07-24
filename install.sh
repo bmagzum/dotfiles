1810743f0dbddb2e15909d5057dca9a034260ce9
mb <keksh01@gmail.com>
2025-07-24 19:43:03 +0200

    Add --noconfirm to package installation

--- install.sh
+++ install.sh
@@ -24,8 +24,8 @@ info "Installing apps"
 install_packages() {
     local packages=("$@")
     for pkg in "${packages[@]}"; do
-        if ! sudo pacman -S "$pkg"; then
-            if ! yay -S "$pkg"; then
+        if ! sudo pacman -S --noconfirm "$pkg"; then
+            if ! yay -S --noconfirm "$pkg"; then
                 error "Failed to install $pkg"
                 exit 1
             fi
